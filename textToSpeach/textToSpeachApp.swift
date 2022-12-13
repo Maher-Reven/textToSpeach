b1ba87e663228906ac1c5660f167f8d79027f067
maher <maher.kurdi@brightfish.com>
2022-12-13 10:45:20 +0100

    Initial Commit

---
+++ textToSpeach/textToSpeachApp.swift
@@ -0,0 +1,17 @@
+//
+//  textToSpeachApp.swift
+//  textToSpeach
+//
+//  Created by Maher on 13/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct textToSpeachApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
